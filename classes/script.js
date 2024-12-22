666d4d90c00623e5dc991573999e21e098ccd67e
Aditya Pant <pantanupam05@gmail.com>
2024-12-22 12:32:39 +0530

    initial commit

---
+++ classes/script.js
@@ -0,0 +1,17 @@
+class rectangle {
+	constructor(width, height, color) {
+		this.width = width;
+		this.height = height;
+		this.color = color;
+	}
+	area() {
+		const area = this.width * this.height;
+		return area;
+	}
+	paint() {
+		console.log(`painting with color ${this.color}`);
+	}
+}
+const rect = new rectangle(2, 4);
+const area = rect.area();
+console.log(area);
